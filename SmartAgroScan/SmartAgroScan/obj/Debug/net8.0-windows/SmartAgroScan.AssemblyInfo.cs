7a2faa0f7a9fbcb2f0e71e10ee2ff20d577aef4d
Marup Hosain <marup.hossain00800@gmail.com>
2025-06-22 00:19:34 +0600

    Admin site Background Change

--- SmartAgroScan/SmartAgroScan/obj/Debug/net8.0-windows/SmartAgroScan.AssemblyInfo.cs
+++ SmartAgroScan/SmartAgroScan/obj/Debug/net8.0-windows/SmartAgroScan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartAgroScan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e48478ffd9b8d055c2d0ec3fca50191be87af5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a8d52473808f682f15e8156021c0ffd78a4264")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartAgroScan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartAgroScan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
